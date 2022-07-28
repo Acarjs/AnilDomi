294c7ff00ca483d796db391397a6d42975f0d4f9
Acarjs <101933654+Acarjs@users.noreply.github.com>
2022-07-28 13:19:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const panels = document.querySelectorAll(".panel");
+
+// panels.forEach(function (panel) {
+//   panel.addEventListener("click", function () {
+//     panel.classList.add("active");
+//   });
+// });
+
+panels.forEach(function (panel) {
+  panel.addEventListener("click", function () {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach(function (panel) {
+    panel.classList.remove("active");
+  });
+}
